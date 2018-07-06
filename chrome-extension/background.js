89048ab42f5ef72b00cdcf98f719984a89d69875
Lucio Paiva <luciopaiva@gmail.com>
2018-07-06 20:46:39 -0300

    Load scripts directly from the extension folder

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,122 +1,137 @@
-"use strict";
-// need to load background.js as persistent in manifest.json, otherwise it will be unloaded and these maps will be lost!
-
-/**
- * Quick doc to help linters
- * @class chrome
- * @property runtime.onMessage.addListener
- * @property tabs.onActivated
- * @property browserAction.setBadgeText
+/*
+    This is the background script, controlling all content scripts running on each tab. Since `manifest.json` is set to
+    background-persistent mode, a single instance will run, guaranteed not to leave memory and thus keeping its state.
  */
 
+// some declarations just to make linters stop complaining
+/**
+ * @class chrome
+ * @property runtime.onMessage.addListener
+ * @property runtime.getURL
+ * @property tabs.onActivated
+ * @property tabs.sendMessage
+ * @property browserAction.setBadgeText
+ */
 /**
  * // https://developer.chrome.com/extensions/tabs#type-Tab
  * @class Tab
- * @property {number} id
+ * @property {Number} id
  */
-
 /**
  * // https://developer.chrome.com/extensions/runtime#type-MessageSender
  * @class MessageSender
  * @property {Tab} tab
- * @property {number} frameId
+ * @property {Number} frameId
  */
 
+class Witchcraft {
 
-class WitchcraftBackgroundManager {
+    static get PATH_TO_SCRIPTS() { return "scripts/"; }
 
     constructor () {
-        /** @type {Map<number, Set<string>>} map with number of scripts loaded per tab */
+        /** @type {Map<number, Set<string>>} map with number of scripts loaded per tab, with the sole purpose of keeping
+         *                                   the badge in the UI up-to-date */
         this.scriptsLoadedByTabId = new Map();
 
         // listen for script/stylesheet requests
-        chrome.runtime.onMessage.addListener(this.retrieveRelatedScriptsFromServer.bind(this));
+        chrome.runtime.onMessage.addListener(this.retrieveRelevantScripts.bind(this));
 
         // listen for tab switches
         chrome.tabs.onActivated.addListener(
             /** @type {{tabId: number}} */ activeInfo => this.updateIconBadge(activeInfo.tabId));
     }
 
-    createOrResetScriptsSetForTab(tabId) {
-        let scripts = this.scriptsLoadedByTabId.get(tabId);
+    /**
+     * @param {MessageSender} sender - the sender context of the content script that called us
+     * @returns {Set<String>}
+     */
+    obtainScriptsSetForSender(sender) {
+        let scripts = this.scriptsLoadedByTabId.get(sender.tab.id);
         if (!scripts) {
             scripts = new Set();
-            this.scriptsLoadedByTabId.set(tabId, scripts);
-        } else {
+            this.scriptsLoadedByTabId.set(sender.tab.id, scripts);
+        }
+
+        if (sender.frameId === 0) {
+            // this is the top frame; assume the tab is being reloaded and take the chance to reset its counter
             scripts.clear();
         }
+
         return scripts;
+    }
+
+    /**
+     * Receives a domain and yields it back in parts, progressively adding sub-levels starting from the TLD. For
+     * instance, if the hostname is `"foo.bar.com"`, the resulting sequence will be `'com'`, `'bar.com'`,
+     * `'foo.bar.com'`.
+     *
+     * @param {String} hostname
+     * @returns {IterableIterator<String>}
+     */
+    static *iterateDomainLevels(hostname) {
+        const parts = hostname.split('.');
+        for (let i = parts.length - 1; i >= 0; i--) {
+            yield parts.slice(i, parts.length).join('.');
+        }
+    }
+
+    /**
+     * @param {String} scriptFileName - the file inside the extension folder to load
+     * @returns {Promise<String>} file contents or null if file does not exist
+     */
+    static getFileFromExtensionFolder(scriptFileName) {
+        return new Promise(resolve => {
+            const request = new XMLHttpRequest();
+            request.addEventListener("load", function () {
+                // script was found - return its contents
+                resolve(this.responseText);
+            });
+            request.addEventListener("error", function () {
+                // scripts does not exit
+                resolve(null);
+            });
+            const extensionUrl = chrome.runtime.getURL(Witchcraft.PATH_TO_SCRIPTS + scriptFileName);
+            request.open("GET", extensionUrl, true);
+            request.send();
+        });
     }
 
     /**
      * Ask the local server to retrieve all relevant scripts for this url.
      *
-     * @param {{ type: string, hostname: string }} parameters - type is either 'css' or 'js' and hostname is the page's
-     * @param {MessageSender} sender - the sender context of the foreground script
-     * @param {Function} callback - will be called back with all scripts bundled into a single string
+     * @param {String} hostName - the host name of the tab being loaded
+     * @param {MessageSender} sender - the sender context of the content script that called us
      */
-    retrieveRelatedScriptsFromServer(parameters, sender, callback) {
+    async retrieveRelevantScripts(hostName, sender) {
+        const scriptsSet = this.obtainScriptsSetForSender(sender);
 
-        let scriptsSet = null;
-        if (sender.frameId === 0) {
-            // this is the top frame of the tab, so take the chance and reset the set of scripts for that tab
-            scriptsSet = this.createOrResetScriptsSetForTab(sender.tab.id);
-        } else {
-            scriptsSet = this.scriptsLoadedByTabId.get(sender.tab.id);
+        for (const domain of Witchcraft.iterateDomainLevels(hostName)) {
+            await Witchcraft.handleScriptLoading(domain, "js", scriptsSet, sender);
+            await Witchcraft.handleScriptLoading(domain, "css", scriptsSet, sender);
         }
 
-        const httpRequest = new XMLHttpRequest();
-
-        httpRequest.addEventListener('readystatechange', () => {
-            if (httpRequest.readyState === XMLHttpRequest.DONE) {
-                this.parseScriptResponse(
-                    httpRequest.status, httpRequest.responseText, sender.tab.id, scriptsSet, callback);
-            }
-        });
-
-        const SVR_PROTOCOL = 'http';
-        const SVR_HOSTNAME = 'localhost';
-        const SVR_PORT = 3131;
-
-        // composes a request to our local server (e.g.: http://localhost:3131/css/github.com)
-        const requestUrl = `${SVR_PROTOCOL}://${SVR_HOSTNAME}:${SVR_PORT}/${parameters.type}/${parameters.hostname}`;
-
-        httpRequest.open('GET', requestUrl);
-        httpRequest.send();
-
-        return true;  // must return true to indicate to our peer that it should keep listening for an asynchronous response
-                      // thanks to https://stackoverflow.com/a/20077854/778272
+        this.updateIconBadge(sender.tab.id);
     }
 
     /**
-     * Process incoming server response with scripts to inject in the foreground.
-     *
-     * @param {number} status - 200 if response is good; something else otherwise
-     * @param {string} responseText - the contents returned by the local server
-     * @param {number} tabId - tab being updated
-     * @param {Set<string>} scriptsSet - the set of scripts loaded by the current tab so far
-     * @param {Function} callback - function to call back to send script to the foreground
+     * @param {String} domain
+     * @param {String} scriptType - either "js" or "css"
+     * @param {Set<String>} scriptsSet - set of scripts to update if this script is successfully loaded
+     * @param {MessageSender} sender - the sender context of the content script that called us
+     * @returns {Promise<void>}
      */
-    parseScriptResponse(status, responseText, tabId, scriptsSet, callback) {
-        let script = '';
-
-        // if we did get valid content
-        if (status === 200) {
-            script = responseText;
-
-            const scriptStartIndex = script.indexOf('\n\n');
-            if (scriptStartIndex !== -1) {
-                script.substring(0, scriptStartIndex).split('\n').forEach(scriptName => scriptsSet.add(scriptName));
-
-                // skip script names before sending final result to the foreground
-                script = script.substring(scriptStartIndex);
-            }
+    static async handleScriptLoading(domain, scriptType, scriptsSet, sender) {
+        const scriptFileName = `${domain}.${scriptType}`;
+        const scriptContents = await Witchcraft.getFileFromExtensionFolder(scriptFileName);
+        if (scriptContents) {
+            chrome.tabs.sendMessage(sender.tab.id, {
+                scriptType,
+                scriptContents,
+            }, {
+                frameId: sender.frameId
+            });
+            scriptsSet.add(scriptFileName);
         }
-
-        // send the script to the foreground
-        callback(script);
-
-        this.updateIconBadge(tabId);
     }
 
     /**
@@ -131,4 +146,4 @@ class WitchcraftBackgroundManager {
     }
 }
 
-new WitchcraftBackgroundManager();
+new Witchcraft();
