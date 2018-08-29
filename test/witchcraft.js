9cf8ea2a6ef0b7c43f2afb98a50c132fb0c97ea9
Lucio Paiva <luciopaiva@gmail.com>
2018-08-29 00:49:56 -0300

    Add test for include directive

--- test/witchcraft.js
+++ test/witchcraft.js
@@ -1,12 +1,29 @@
 
 const
     assert = require("assert"),
-    {describe, it} = require("mocha"),
+    {describe, it, setup} = require("mocha"),
     sinon = require("sinon"),
     chrome = require("sinon-chrome"),
     Witchcraft = require("../chrome-extension/witchcraft");
 
 describe("Witchcraft", function () {
+
+    const tabId = 42;
+    const sampleCode = "// some javascript code";
+
+    /** @type {Witchcraft} */
+    let witchcraft;
+    let sender;
+
+    setup(function () {
+        witchcraft = new Witchcraft(chrome, undefined);
+        sinon.stub(witchcraft, "queryLocalServerForFile").resolves(sampleCode);
+
+        sender = {
+            tab: { id: tabId },
+            frameId: 0
+        };
+    });
 
     it ("should correctly iterate domain levels", function () {
         let levels = [...Witchcraft.iterateDomainLevels("www.google.com")];
@@ -44,7 +61,6 @@ describe("Witchcraft", function () {
     });
 
     it("should not add same script twice for same tab", function () {
-        const witchcraft = new Witchcraft(chrome, undefined);
         assert.strictEqual(witchcraft.getScriptNamesForTabId(1).size, 0);
         witchcraft.registerScriptForTabId("foo", 1);
         assert.strictEqual(witchcraft.getScriptNamesForTabId(1).size, 1);
@@ -53,8 +69,6 @@ describe("Witchcraft", function () {
     });
 
     it("should update interface for given tab", function () {
-        const witchcraft = new Witchcraft(chrome, undefined);
-
         sinon.spy(witchcraft, "updateIconWithScriptCount");
 
         // non-existing tab id, should report zero scripts
@@ -81,14 +95,6 @@ describe("Witchcraft", function () {
     });
 
     it("should load single script", async function () {
-        const tabId = 42;
-        const sampleCode = "// some javascript code";
-        const witchcraft = new Witchcraft(chrome, undefined);
-        sinon.stub(witchcraft, "queryLocalServerForFile").resolves(sampleCode);
-        const sender = {
-            tab: { id: tabId },
-            frameId: 0
-        };
         await witchcraft.loadScript("google.com", "js", sender);
 
         // check that the message is being sent to the tab
@@ -105,18 +111,8 @@ describe("Witchcraft", function () {
     });
 
     it("should fetch all relevant scripts for hostname", async function () {
-        const tabId = 42;
-        const sampleCode = "// some javascript code";
         const globalJs = Witchcraft.globalScriptName + ".js";
         const globalCss = Witchcraft.globalScriptName + ".css";
-        const witchcraft = new Witchcraft(chrome, undefined);
-
-        sinon.stub(witchcraft, "queryLocalServerForFile").resolves(sampleCode);
-
-        const sender = {
-            tab: { id: tabId },
-            frameId: 0
-        };
 
         await witchcraft.onScriptRequest("google.com", sender);
 
@@ -133,5 +129,39 @@ describe("Witchcraft", function () {
             globalJs, globalCss,
             "com.js", "com.css",
             "google.com.js", "google.com.css"]);
+    });
+
+    it("should process include directives", async function () {
+        const includeFoo = "// @include foo.js";
+        const sampleCodeWithIncludeDirective = `console.info('Hello');\n${includeFoo}\nconsole.info('world');`;
+        const fooCode = "// foo";
+        const startIndex = sampleCodeWithIncludeDirective.indexOf(includeFoo);
+        const endIndex = startIndex + includeFoo.length;
+        const finalCode = Witchcraft.spliceString(sampleCodeWithIncludeDirective, startIndex, endIndex, fooCode);
+
+        witchcraft.queryLocalServerForFile.reset();
+        let callIndex = -1;
+        witchcraft.queryLocalServerForFile.onCall(++callIndex).resolves(null);  // _global.js
+        witchcraft.queryLocalServerForFile.onCall(++callIndex).resolves(null);  // _global.css
+        witchcraft.queryLocalServerForFile.onCall(++callIndex).resolves(sampleCodeWithIncludeDirective);  // com.js
+        witchcraft.queryLocalServerForFile.onCall(++callIndex).resolves(fooCode);  // foo.js (included from com.js)
+        witchcraft.queryLocalServerForFile.onCall(++callIndex).resolves(null);  // com.css
+        await witchcraft.onScriptRequest("com", sender);
+
+        const calls = witchcraft.queryLocalServerForFile.getCalls();
+        assert.strictEqual(calls.length, 5);
+        const requestedScripts = calls.map(call => call.args[0]);
+        assert.deepStrictEqual(requestedScripts, [
+            "_global.js",
+            "_global.css",
+            "com.js",
+            "foo.js",
+            "com.css",
+        ]);
+
+        assert(chrome.tabs.sendMessage.calledOnce);
+        const tabMessageCall = chrome.tabs.sendMessage.getCall(0);
+        assert.strictEqual(tabMessageCall.args.length, 3);
+        assert.strictEqual(tabMessageCall.args[1].scriptContents, finalCode);
     });
 });
