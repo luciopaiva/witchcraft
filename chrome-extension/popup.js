476071f74c106fa443220e796e60ae23b0752d9b
Lucio Paiva <luciopaiva@gmail.com>
2020-02-29 17:22:24 +0000

    Popup: add version, links to docs and issues

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -11,54 +11,79 @@
  * @property chrome.browserAction.setTitle
  */
 
-(function populatePopup() {
-    // this script will run every time the popup is shown (i.e., every time the user clicks on the extension icon)
-    // it reads information from the background window and shows to the user
+class Popup {
 
-    const background = chrome.extension.getBackgroundPage();
+    constructor () {
+        const background = chrome.extension.getBackgroundPage();
 
-    /** @type {Witchcraft} */
-    const witchcraft = background.window.witchcraft;
+        /** @type {Witchcraft} */
+        this.witchcraft = background.window.witchcraft;
 
-    const scriptsTable = document.getElementById("scripts-table");
-    const noScriptsElement = document.getElementById("no-scripts");
-
-    const scriptNames = witchcraft.getCurrentTabScriptNames();
-
-    const serverAddress = witchcraft.getServerAddress();
-
-    if (scriptNames && scriptNames.size > 0) {
-        noScriptsElement.classList.add("hidden");
-        scriptsTable.classList.remove("hidden");
-
-        for (const scriptName of scriptNames) {
-
-            const tdName = document.createElement("td");
-            const aName = document.createElement("a");
-            tdName.appendChild(aName)
-            aName.target = "_blank";
-            aName.innerText = scriptName;
-            aName.href = serverAddress + scriptName;
-
-            const tdType = document.createElement("td");
-            const extensionMatch = scriptName.match(/\.([^.]+)$/);
-            if (extensionMatch) {
-                const extension = extensionMatch[1];
-                tdType.classList.add(extension.toLowerCase());
-                tdType.innerText = extension.toUpperCase();
-            } else {
-                tdType.innerText = "?";
-            }
-
-            const tr = document.createElement("tr");
-            tr.appendChild(tdName);
-            tr.appendChild(tdType);
-            scriptsTable.appendChild(tr);
-        }
-    } else {
-        noScriptsElement.classList.remove("hidden");
-        scriptsTable.classList.add("hidden");
+        this.makeButton("docs");
+        this.makeButton("report-issue");
+        this.showVersion();
+        this.showServerStatus();
+        this.renderScriptsTable();
     }
 
-    document.getElementById("server-status").classList.toggle("offline", !witchcraft.isServerReachable);
-})();
+    showVersion() {
+        document.getElementById("version").innerText = chrome.runtime.getManifest().version;
+    }
+
+    makeButton(id) {
+        const link = document.getElementById(id);
+        link.addEventListener("click", () => {
+            chrome.tabs.create({ url: link.getAttribute("href") });
+            return false;
+        });
+    }
+
+    showServerStatus() {
+        document.getElementById("server-status")
+            .classList.toggle("offline", !this.witchcraft.isServerReachable);
+    }
+
+    renderScriptsTable() {
+        const scriptsTable = document.getElementById("scripts-table");
+        const noScriptsElement = document.getElementById("no-scripts");
+
+        const scriptNames = this.witchcraft.getCurrentTabScriptNames();
+
+        const serverAddress = this.witchcraft.getServerAddress();
+
+        const hasScripts = scriptNames && scriptNames.size > 0;
+        noScriptsElement.classList.toggle("hidden", hasScripts);
+        scriptsTable.classList.toggle("hidden", !hasScripts);
+
+        if (hasScripts) {
+            for (const scriptName of scriptNames) {
+
+                const tdName = document.createElement("td");
+                const aName = document.createElement("a");
+                tdName.appendChild(aName);
+                aName.target = "_blank";
+                aName.innerText = scriptName;
+                aName.href = serverAddress + scriptName;
+
+                const tdType = document.createElement("td");
+                const extensionMatch = scriptName.match(/\.([^.]+)$/);
+                if (extensionMatch) {
+                    const extension = extensionMatch[1];
+                    tdType.classList.add(extension.toLowerCase());
+                    tdType.innerText = extension.toUpperCase();
+                } else {
+                    tdType.innerText = "?";
+                }
+
+                const tr = document.createElement("tr");
+                tr.appendChild(tdName);
+                tr.appendChild(tdType);
+                scriptsTable.appendChild(tr);
+            }
+        }
+    }
+}
+
+// this script will run every time the popup is shown (i.e., every time the user clicks on the extension icon)
+// it reads information from the background window and shows it to the user
+window.addEventListener("load", () => new Popup());
