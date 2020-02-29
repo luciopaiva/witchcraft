6cd2bd7441dcfdb46afc1c3e09b703e510d06e07
Lucio Paiva <luciopaiva@gmail.com>
2020-02-29 03:07:18 +0000

    Correctly process CSS include directives
    This fixes #3. Also refactored the whole include logic so it can be properly tested.

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,7 +1,9 @@
 
 // bind it to window so it can be accessed from the popup screen
-window.analytics = new Analytics();
-window.analytics.send("Background", "Load");
+if (typeof Analytics === "function") {  // will be undefined during tests
+    window.analytics = new Analytics();
+    window.analytics.send("Background", "Load");
+}
 
 // bind it to window so it can be accessed from the popup screen
 window.witchcraft = new Witchcraft(chrome, typeof document !== "undefined" ? document : undefined, window.analytics);
