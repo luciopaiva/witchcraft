45a01f84a9335f9ebf3dd92ecaa6b9c1818ce128
Gerald Gillespie <gg@suitegeezus.com>
2022-09-05 00:42:47 -0400

    fix bug where sometimes scripts would not load because event has already fired

--- chrome-extension/content-script.js
+++ chrome-extension/content-script.js
@@ -1,6 +1,8 @@
-/*
- This is the part that gets injected in each frame of the page. It signals the Witchcraft background script that a new
- frame has been loaded and gets ready (by creating a listener) to receive and load any matching scripts.
+/**
+ * @description This is the part that gets injected in each frame of the page. It signals the Witchcraft background
+ * script that a new frame has been loaded and gets ready (by creating a listener) to receive and load any matching
+ * scripts.
+ * document.readyState works like this {@link https://developer.mozilla.org/en-US/docs/Web/API/Document/readyState|mdn}
  */
 
 chrome.runtime.onMessage.addListener(({ scriptType, scriptContents, scriptMode = 0 }) => {
@@ -12,7 +14,7 @@ chrome.runtime.onMessage.addListener(({ scriptType, scriptContents, scriptMode =
       scriptEl.setAttribute("data-witchcraft", "");  // to make it easy finding the element if we want to
       document[node].appendChild(scriptEl);
     };
-
+    const {readyState} = document;
     let ev, node;
     switch ( scriptMode ){
       case 0:
@@ -22,10 +24,13 @@ chrome.runtime.onMessage.addListener(({ scriptType, scriptContents, scriptMode =
         return createScriptEl({ node, scriptContents });
       case 2:
         [ ev, node ] = [ 'load', 'body' ];
-        return window.addEventListener(ev, () => ( createScriptEl({ node, scriptContents }) ));
+        if( readyState !== 'complete' ) return window.addEventListener(ev, () => ( createScriptEl({ node, scriptContents }) ));
+        return createScriptEl({node, scriptContents});
       case 3:
+        // if the readyState is loading then the next state of 'interactive' has not happened yet.
         [ ev, node ] = [ 'DOMContentLoaded', 'body' ];
-        return document.addEventListener(ev, () => ( createScriptEl({ node, scriptContents }) ));
+        if( readyState ==='loading') return document.addEventListener(ev, () => ( createScriptEl({ node, scriptContents }) ));
+        return createScriptEl({node,scriptContents});
     }
 
   } else if ( scriptType === "css" ){
