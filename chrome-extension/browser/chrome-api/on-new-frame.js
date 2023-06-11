e9ae6076c926a0e5be6826fab3ee84f682a4b9d3
Lucio Paiva <luciopaiva@gmail.com>
2023-06-11 19:08:32 +0100

    Remove unnecessary async keyword from sync functions

--- chrome-extension/browser/chrome-api/on-new-frame.js
+++ chrome-extension/browser/chrome-api/on-new-frame.js
@@ -2,6 +2,6 @@ import {chromeApi} from "./index.js";
 
 const NAVIGATION_FILTER = { url: [{ schemes: ["http", "https", "file", "ftp"] }] };
 
-export async function onNewFrame(callback) {
+export function onNewFrame(callback) {
     chromeApi.chrome().webNavigation.onCommitted.addListener(callback, NAVIGATION_FILTER);
 }
