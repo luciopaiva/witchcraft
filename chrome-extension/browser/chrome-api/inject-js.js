18512fa14aa0fcc0270d6572d4ba5682cc34ef74
Lucio Paiva <luciopaiva@gmail.com>
2025-03-14 15:20:28 +0000

    Inject script as early as possible

--- chrome-extension/browser/chrome-api/inject-js.js
+++ chrome-extension/browser/chrome-api/inject-js.js
@@ -2,6 +2,7 @@ import {chromeApi} from "./index.js";
 
 export function injectJs(contents, tabId, frameId) {
     chromeApi.chrome().scripting.executeScript({
+        injectImmediately: true,
         target: { tabId: tabId, frameIds: [frameId] },
         func: (contents) => Function(contents)(),
         args: [contents],
