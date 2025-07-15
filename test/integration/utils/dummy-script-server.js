df6f049cc4e44725a6e5117b51f50e459d1d4ed7
Lucio Paiva <luciopaiva@gmail.com>
2025-07-15 21:56:19 +0100

    Add scripts to dummy script server

--- test/integration/utils/dummy-script-server.js
+++ test/integration/utils/dummy-script-server.js
@@ -4,14 +4,32 @@ class DummyScriptServer {
     constructor() {
         this.server = null;
         this.port = null;
+        this.scripts = new Map();
+    }
+
+    addScript(scriptPath, scriptContents) {
+        this.scripts.set(scriptPath, scriptContents);
     }
 
     async start() {
         return new Promise((resolve, reject) => {
             this.server = http.createServer((req, res) => {
                 console.log(`Request received for path: ${req.url}`);
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end('OK');
+
+                if (this.scripts.has(req.url)) {
+                    let contentType = 'text/plain';
+                    if (req.url.endsWith('.js')) {
+                        contentType = 'application/javascript';
+                    } else if (req.url.endsWith('.css')) {
+                        contentType = 'text/css';
+                    }
+
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(this.scripts.get(req.url));
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('Not Found');
+                }
             });
 
             this.server.listen(0, () => {
