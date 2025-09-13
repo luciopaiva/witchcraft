ed2f65892855546e44bcadf6daaf30e1509f4be0
Lucio Paiva <luciopaiva@gmail.com>
2025-09-13 18:09:43 +0100

    Documentation WIP

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from "starlight-theme-rapide";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,18 +12,28 @@ export default defineConfig({
                 src: "./src/assets/title.png",
             },
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/luciopaiva/witchcraft' }],
+            plugins: [starlightThemeRapide()],
+			customCss: [
+				'./src/styles/global.css',
+			],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+                { label: 'Introduction', slug: 'introduction' },
+                { label: 'How to install', slug: 'how-to-install' },
+                { label: 'New in version 3', slug: 'new-in-v3' },
+                { label: 'Features', slug: 'features' },
+                { label: 'Examples', slug: 'examples' },
+                { label: 'FAQ', slug: 'faq' },
+                { label: 'Scripts server', slug: 'scripts-server' },
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
 			],
 		}),
 	],
