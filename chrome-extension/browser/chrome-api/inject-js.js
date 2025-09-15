5f9d28fdd6121c2637ef4537511e44d03b010636
Lucio Paiva <luciopaiva@gmail.com>
2025-09-15 18:25:48 +0100

    Prefer userScripts, fallback to scripting

--- chrome-extension/browser/chrome-api/inject-js.js
+++ chrome-extension/browser/chrome-api/inject-js.js
@@ -1,11 +1,28 @@
 import {chromeApi} from "./index.js";
 
+function isUserScriptsEnabled() {
+    return typeof chrome?.userScripts?.execute === "function";
+}
+
 export function injectJs(contents, tabId, frameId) {
-    chromeApi.chrome().scripting.executeScript({
-        injectImmediately: true,
-        target: { tabId: tabId, frameIds: [frameId] },
-        func: (contents) => Function(contents)(),
-        args: [contents],
-        world: "MAIN"
-    }).catch(chromeApi.captureRuntimeError);
+    const enabled = isUserScriptsEnabled();
+    console.info(`DEBUG: injectJs using ${enabled ? "userScripts" : "scripting"}`);
+    if (enabled) {
+        chrome.userScripts.execute({
+            injectImmediately: true,
+            target: { tabId: tabId, frameIds: [frameId] },
+            js: [{
+                code: contents,
+            }],
+            world: "MAIN"
+        }).catch(chromeApi.captureRuntimeError);
+    } else {
+        chromeApi.chrome().scripting.executeScript({
+            injectImmediately: true,
+            target: { tabId: tabId, frameIds: [frameId] },
+            func: (contents) => Function(contents)(),
+            args: [contents],
+            world: "MAIN"
+        }).catch(chromeApi.captureRuntimeError);
+    }
 }
