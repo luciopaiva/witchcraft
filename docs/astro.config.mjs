ebd24ee079aa99841d68e71e56bee252f439688f
Lucio Paiva <luciopaiva@gmail.com>
2025-09-16 17:08:16 +0100

    Fix all links

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,12 +1,58 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeRapide from "starlight-theme-rapide";
+
+/** Quick and dirty plugin that:
+ *  - Throws an error if relative links are found
+ *  - Prepends /witchcraft to all absolute links (starting with /)
+ */
+function remarkAbsoluteLinksSimple() {
+    const prefix = `[absolute-links]`;
+    let indentLevel = 0;
+    const indent = () => indentLevel += 2;
+    const dedent = () => indentLevel -= 2;
+    const log = (msg) => console.info(`${prefix}${"  ".repeat(indentLevel)} ${msg}`);
+    return (tree, file) => {
+        log(`Processing file: ${file.path}`);
+
+        function visit(node) {
+            if (!node || typeof node !== "object") return;
+
+            indent();
+            // log(`node: type=${node.type} children=${Array.isArray(node.children) ? node.children.length : 0}`);
+
+            if (node.type === "link" && typeof node.url === "string") {
+                const url = node.url;
+                log(`link: ${url}`);
+
+                // fail on relative links
+                if (node.url.startsWith(".")) {
+                    throw new Error(`Relative links starting with './' are not supported. Please use root-relative links starting with '/' instead. Found in file ${file.path}: ${node.url}`);
+                }
+
+                // prepend base to all absolute links (doesn't touch external links)
+                if (node.url.startsWith('/')) {
+                    node.url = `/witchcraft${node.url}`;
+                }
+            }
+
+            for (const child of node.children || []) {
+                visit(child);
+            }
+
+            dedent();
+        }
+        visit(tree);
+    }
+}
 
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.ASTRO_SITE || 'https://luciopaiva.com',
 	base: process.env.ASTRO_BASE || '/witchcraft/',
+	markdown: {
+        remarkPlugins: [remarkAbsoluteLinksSimple],
+	},
 	integrations: [
 		starlight({
 			title: '',
